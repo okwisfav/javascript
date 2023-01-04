ea8b0dfc34594af5f4850fe5df1f500a3cda8707
ofego favour <ofegofavour@gmail.com>
2023-01-04 12:16:47 +0100

    variables & block scope

--- javascript.js
+++ javascript.js
@@ -125,10 +125,61 @@
 
 // logical Not (!)
 
-let user = false;
-if(!user){
-    console.log('you must be logged in to continue');
-}
+// let user = false;
+// if(!user){
+//     console.log('you must be logged in to continue');
+// }
 
-console.log(!true)
-console.log(!false);
+// console.log(!true)
+// console.log(!false);
+
+//break and continue
+// const scores = [50, 25, 0, 30, 100, 20, 10];
+// for(let i = 0; i < scores.length; i++){
+//     if(scores[i] === 0){
+//         continue;
+//     }
+
+//     console.log('your score: ', scores[i]);
+//    if(scores[i] === 100){
+//       console.log('congrats, you got the top score!');
+//       break;
+//    }
+// }
+
+// switch statements 
+// const grade = 'A';
+// switch(grade){
+//     case 'A':
+//       console.log('you got an A!');
+//       break;
+//     case 'B':
+//       console.log('you got an B!');
+//       break;
+//     case 'C':
+//         console.log('you got an C!');
+//         break;
+//     case 'D':
+//          console.log('you got an D!');
+//          break;
+//     case 'E':
+//          console.log('you got an D!');
+//          break;
+//     default:
+//         console.log('not a valid grade');
+// }
+
+//variables & block scope 
+// let age = 30;
+
+// if(true){
+//  let age = 40;
+//  let name = 'favour'
+//    console.log('inside 1st code block', age, name); 
+
+//    if(true){
+//     console.log('inside 2nd code block: ', age);
+//    }
+// }
+
+// console.log('outside code block:', age);
