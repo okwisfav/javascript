f5ac596e7a808ef5a03f7aeb16438cf357b4498b
ofego favour <ofegofavour@gmail.com>
2023-01-04 09:56:37 +0100

    locgical operation

--- javascript.js
+++ javascript.js
@@ -111,13 +111,13 @@
 // }
 
 //locgical operation - OR || and AND &&
-    const password = 'p@ass';
+    const password = 'p@ass12';
 
-    if(password.length >= 12){
+    if(password.length >= 12 && password.includes('@')){
         console.log('that password is mighty strong');
     }
-    else if(password.length >= 8){
-        console.log('that password is long enough!');
+    else if(password.length >= 8 || password.includes('@') && password.length >= 5){
+        console.log('that password is strong enough!');
     } else {
-        console.log('password is not long enough');
+        console.log('password is not strong enough');
     }
