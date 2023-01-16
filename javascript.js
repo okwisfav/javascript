06db21644e10a94f4aafdd4d3eb1ba2b7ef696be
ofego favour <ofegofavour@gmail.com>
2023-01-16 21:05:23 +0100

    EventListener

--- javascript.js
+++ javascript.js
@@ -183,3 +183,169 @@
 // }
 
 // console.log('outside code block:', age);
+
+// function declaration
+
+// function greet(){
+//     console.log('hello there');
+// }
+
+// function expression 
+// const speak = function(){
+//   console.log("good-day ma")
+// };
+// speak()
+
+// greet();
+
+//argument & parameters 
+// const speak = function(name = 'favour', time = 'night'){
+//     console.log(`good ${time} ${name}`);
+// };
+
+// speak();
+
+// retuning values 
+// const calcArea = function(radius){
+//     let area = 3.14 * radius**2;
+//     return area;
+// }
+
+// const area = calcArea(5);
+// console.log(area);
+
+//arrow function 
+// const calcArea = (radius) => {
+//     return 3.14 * radius**2;
+// };
+
+// const area = calcArea(5);
+// console.log('area is', area)
+
+//object 
+
+
+// console.log(blogs)
+// let user = {
+//     name: 'favour',
+//     age: 30,
+//     email: 'favourofego@gmail.com',
+//     location: 'berlin',
+//      blogs: [
+//       {title: 'why mac & cheese rules', likes: 30},
+//       {title: '10 things to make with marmite', likes: 50}
+//      ],
+//      login: function(){
+//         console.log('the user logged in');
+//      },
+//      logout: function(){
+//        console.log('the user logged out');
+//      },
+
+//      logBlogs(){
+//         console.log('this user has written the following blogs:');
+//         this.blogs.forEach(blog => {
+//           console.log(blog.title, blog.likes);
+//         })
+//      }
+// };
+// user.logBlogs();
+// console.log(this)
+
+// Math object
+
+// console.log(Math);
+// console.log(Math.PI);
+// console.log(Math.E);
+
+// const area = 7.7;
+
+// console.log(Math.round(area));
+
+// const random = Math.random();
+
+// console.log(random);
+
+// let scoreOne = 50;
+// let scoreTwo = scoreOne;
+
+// console.log(`scoreOne: ${scoreOne}`, `scoreTwo: ${scoreTwo}`);
+
+// scoreOne = 100;
+
+// console.log(`scoreOne: ${scoreOne}`, `scoreTwo: ${scoreTwo}`);
+
+// const userOne = { name: 'favour', age: 30};
+// const userTwo = userOne;
+
+// console.log(userOne, userTwo);
+
+// userOne.name = 'ofego';
+// console.log(userOne, userTwo);
+
+// const para = document.querySelector('p');
+
+// console.log(para)
+
+// const paras = document.querySelectorAll('p');
+
+// console.log(paras);
+
+// get an element by ID 
+// const title = document.getElementById('page-title');
+// console.log(title);
+
+// get element by their class name 
+// const errors = document.getElementsByClassName('error');
+// console.log(errors);
+
+//get elements by their tag name 
+// const paras = document.getElementsByTagName('p');
+// console.log(paras[1]);
+
+// const para = document.querySelector('p');
+// console.log(para.innerText);
+// para.innerText = 'try to stop smoke weed';
+
+// const paras = document.querySelectorAll('p');
+
+//for changing innertext
+// paras.forEach(para => {
+//     console.log(para.innerText);
+//     para.innerText += 'new text'
+// })
+
+// const content = document.querySelector('.content');
+// const people = ['favour', 'john', 'oke'];
+// people.forEach(person => {
+//     content.innerHTML += `<p>${person}</p>`;
+// });
+
+// const link = document.querySelector('a');
+// console.log(link.getAttribute('href'));
+// link.setAttribute('href', 'https://www.thenetninja.co.uk');
+
+// const title = document.querySelector('h1');
+// title.setAttribute('style', 'margin: 50px;');
+
+// const content = document.querySelector('p');
+
+// console.log(content.classList);
+// content.classList.add('success');
+
+// const button = document.querySelector('button');
+
+// button.addEventListener(`click`, () => {
+//     console.log('you clicked me');
+// });
+
+const ul = document.querySelector('ul')
+ul.remove();
+
+const items = document.querySelectorAll('li');
+items.forEach(item => {
+    item.addEventListener('click', e => {
+        e.target.style.textDecoration = 'line-through';
+    });
+})
+
